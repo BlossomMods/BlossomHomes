dc51ef56f5404590b90334e826d94547dedbd1a8
codedsakura <sakuracoded@gmail.com>
2022-06-08 14:03:52 +0300

    update config

--- src/main/java/dev/codedsakura/blossom/homes/BlossomHomesConfig.java
+++ src/main/java/dev/codedsakura/blossom/homes/BlossomHomesConfig.java
@@ -1,4 +1,12 @@
 package dev.codedsakura.blossom.homes;
 
+import dev.codedsakura.blossom.lib.TeleportConfig;
+
 public class BlossomHomesConfig {
+    TeleportConfig teleportation = null;
+
+    int standStill = 3;
+    int cooldown = 30;
+
+    int startHomes = 2;
 }
