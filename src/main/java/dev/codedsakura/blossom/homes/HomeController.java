515993665f4a8829c8a3862c10fb9b81d6831851
codedsakura <sakuracoded@gmail.com>
2022-06-09 20:55:33 +0300

    add suggestion provider to HomeController

--- src/main/java/dev/codedsakura/blossom/homes/HomeController.java
+++ src/main/java/dev/codedsakura/blossom/homes/HomeController.java
@@ -1,8 +1,14 @@
 package dev.codedsakura.blossom.homes;
 
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import dev.codedsakura.blossom.lib.ListDataController;
 import dev.codedsakura.blossom.lib.TeleportUtils;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -10,6 +16,7 @@ import net.minecraft.util.registry.RegistryKey;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 class Home {
     public String name;
@@ -58,11 +65,11 @@ class Home {
 
 class PlayerWithHomes {
     public UUID uuid;
-    public Home[] homes;
+    public ArrayList<Home> homes;
     public int maxHomes;
 }
 
-public class HomeController extends ListDataController<PlayerWithHomes> {
+public class HomeController extends ListDataController<PlayerWithHomes> implements SuggestionProvider<ServerCommandSource> {
     @Override
     public Class<PlayerWithHomes[]> getArrayClassType() {
         return PlayerWithHomes[].class;
@@ -76,5 +83,18 @@ public class HomeController extends ListDataController<PlayerWithHomes> {
     @Override
     public String getFilename() {
         return "BlossomHomes";
+    }
+
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
+        UUID player = context.getSource().getPlayer().getUuid();
+        String start = builder.getRemaining().toLowerCase();
+        data.stream()
+                .filter(v -> v.uuid.equals(player))
+                .flatMap(v -> v.homes.stream().map(home -> home.name))
+                .sorted(String::compareToIgnoreCase)
+                .filter(pair -> pair.toLowerCase().startsWith(start))
+                .forEach(builder::suggest);
+        return builder.buildFuture();
     }
 }
